2fc9788c2ab254cbfb95339a77356dd52543b981
Seatwoom <pashchenko.alr@gmail.com>
2026-03-25 16:34:28 +0200

    Fix: remove --env-file from start script to avoid Docker crash

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,11 @@ require("dotenv").config({ silent: true });
 module.exports = {
   client: "pg",
   connection: {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
   migrations: {
     directory: "./migrations",
